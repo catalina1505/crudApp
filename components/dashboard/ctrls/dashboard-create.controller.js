2ebaa48aa13b6bfa210a8caf0c607e41a2fe1e11
catalina1505 <catalina150592@gmail.com>
2018-08-19 23:23:36 +0300

    small update

--- components/dashboard/ctrls/dashboard-create.controller.js
+++ components/dashboard/ctrls/dashboard-create.controller.js
@@ -22,20 +22,21 @@
 						) 
 	{
 		var vm = this;
-		vm.date = date;
 		vm.newProduct = {};	
 				
 		// create and push a new product into dataHolder
         vm.saveProduct = function(){
 		   dataHolder.products.push(vm.newProduct);
-		   vm.date = new Date();
 		   	// generate an unique code: date in milliseconds + random number
 		   vm.newProduct.code = Date.now() + Math.random().toString(16).slice(2,5);
 		   ngNotify.set('You added a new product!', 'success')
 		   $timeout(function() {
 				return $localstorage.setObject('products', dataHolder.products);
 			}, 2000);
+			console.log($localstorage.setObject('products'));
+
 			vm.newProduct = {};
+
 		};	
 	}
 })();

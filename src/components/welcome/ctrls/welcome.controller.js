1795676cd48381cd7377ee95e2f03b154f27d957
catalina1505 <catalina150592@gmail.com>
2018-09-11 01:25:10 +0300

    login done

--- src/components/welcome/ctrls/welcome.controller.js
+++ src/components/welcome/ctrls/welcome.controller.js
@@ -8,43 +8,36 @@
 					Controller);
     
     Controller.$inject = ['$localstorage',
-                        '$state'
+                        '$state',
+                        'ngNotify'
     ];
     
      function Controller($localstorage,
-                        $state
+                        $state,
+                        ngNotify
                          )
     { 
         var vm = this;
-        // Name and Password from the register-form
-        vm.name1 = document.getElementById('name1');
-        vm.pw = document.getElementById('pw');
-        
-        // storing input from register-form
-        function store() {
-            $localstorage.set('name1', name1.value);
-            $localstorage.set('pw', pw.value);
-        }
-        store();
-        
-        // check if stored data from register-form is equal to entered data in the   login-form
+        vm.check = check;
+
+        // save email and password
+        vm.storedName = $localstorage.set('userName', 'catalina@gmail.com');
+        vm.userPw = $localstorage.set('userPw', 'password');
+
+        //check if email and pasword are correct
         function check() {
+    
+            vm.storedName = $localstorage.get('userName');
+            vm.storedPw = $localstorage.get('userPw');
         
-            // stored data from the register-form
-            vm.storedName = $localstorage.get('name1');
-            vm.storedPw = $localstorage.get('pw');
-        
-            // entered data from the login-form
             vm.userName = document.getElementById('userName');
             vm.userPw = document.getElementById('userPw');
 
             if(vm.userName.value == vm.storedName && vm.userPw.value == vm.storedPw) {
-                //$state.go('dashboard');
-                console.log('right');
-            }else {
-                alert('ERROR.');
+                $state.go('dashboard');
+            } else {
+                ngNotify.set('Wrong email or password!', 'error');
             }
         }
-        check()
     } 
 })();

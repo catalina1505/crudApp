eb53bee846c2d1edd3a33bffda04d1ec2cb7535b
Catalina <catalina.postolache@mindit.io>
2018-09-11 17:31:20 +0300

    update

--- src/components/dashboard/ctrls/dashboard.controller.js
+++ src/components/dashboard/ctrls/dashboard.controller.js
@@ -31,6 +31,10 @@
 		vm.viewProd = viewProd;
 		vm.editProd = editProd;
 		vm.submit = submit;
+		vm.pagedItems = [];
+		vm.filteredItems = {};
+		vm.itemsPerPage = 5;
+		vm.groupedItems = [];
 
 		function submit() {
 			$state.go('welcome');
@@ -48,9 +52,53 @@
 		init();
 
 		// pagination
-		vm.numberOfPages = function(){
-			return Math.ceil(vm.prod.length /vm.pageSize);                
-		}
+		// vm.numberOfPages = function(){
+		// 	return Math.ceil(vm.prod.length /vm.pageSize);                
+		// }
+
+// calculate page in place
+    vm.groupToPages = function () {
+        vm.pagedItems = [];
+        
+        for (var i = 0; i < vm.filteredItems.length; i++) {
+            if (i % vm.itemsPerPage === 0) {
+                vm.pagedItems[Math.floor(i / vm.itemsPerPage)] = [ vm.filteredItems[i] ];
+            } else {
+                vm.pagedItems[Math.floor(i / vm.itemsPerPage)].push(vm.filteredItems[i]);
+            }
+        }
+    };
+    
+    vm.range = function (start, end) {
+        var ret = [];
+        if (!end) {
+            end = start;
+            start = 0;
+        }
+        for (var i = start; i < end; i++) {
+            ret.push(i);
+        }
+        return ret;
+    };
+    
+    vm.prevPage = function () {
+        if (vm.currentPage > 0) {
+            vm.currentPage--;
+        }
+    };
+    
+    vm.nextPage = function () {
+        if (vm.currentPage <vm.pagedItems.length - 1) {
+            vm.currentPage++;
+        }
+    };
+    
+    vm.setPage = function () {
+        vm.currentPage = this.n;
+    };
+
+
+
 
 		function selectProduct(product) {
 			vm.selected = product;
